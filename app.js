66ea1b75b22312cc44cec163baf59b35a30a5307
lettuce <lettuce.lu@lettucelu.com>
2021-06-06 00:52:52 +0800

    feat: add user modules

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
+require('./config/mongoose')
 const category_image = ['<i class="fas fa-home"></i>',
   '<i class="fas fa-shuttle-van"></i>',
   '<i class="fas fa-grin-beam"></i>',
@@ -27,25 +28,8 @@ app.use(express.static('public'))
 const routes = require('./routes')
 
 
-// 資料庫設定
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list'
-const mongoose = require('mongoose') // 載入 mongoose
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 const Record = require('./models/record')
-
 
 // 設定 body-parser
 // 引用 body-parser
@@ -53,10 +37,7 @@ const bodyParser = require('body-parser')
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 
-
 app.use(routes)
-
-
 
 
 app.listen(PORT, () => {
