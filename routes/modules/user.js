6f032ee580e453e00a3a468769f585df5bcf8b2e
lettuce <lettuce.lu@lettucelu.com>
2021-06-06 16:02:44 +0800

    feat: add bcryptjs

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -6,7 +6,7 @@ const router = express.Router()
 const User = require('../../models/user')
 
 const passport = require('passport')
-
+const bcrypt = require('bcryptjs')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -31,6 +31,7 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   // 取得註冊表單參數
   const { name, email, password, confirmPassword } = req.body
+  console.log('name:' + name + ' email: ' + email + ' password: ' + password + ' confirmPassword: ' + password)
   // 檢查使用者是否已經註冊
   const errors = []
   if (!name || !email || !password || !confirmPassword) {
@@ -52,24 +53,31 @@ router.post('/register', (req, res) => {
     // 如果已經註冊：退回原本畫面
     if (user) {
       errors.push({ message: '這個 Email 已經註冊過了。' })
-      console.log('User already exists.')
       res.render('register', {
+        errors,
         name,
         email,
         password,
         confirmPassword
       })
-    } else {
-      // 如果還沒註冊：寫入資料庫
-      return User.create({
+
+    }
+    return bcrypt
+      .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+      .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+      .then(hash => User.create({
         name,
         email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
-    }
+        password: hash // 用雜湊值取代原本的使用者密碼
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
   })
+
+
+
+
+
 })
 
 
