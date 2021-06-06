b17bcf06072613762d58ec5f045051ff3f785825
lettuce <lettuce.lu@lettucelu.com>
2021-06-06 14:37:35 +0800

    feat: add user authentication in record

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-
+const { authenticator } = require('../middleware/auth')
 const home = require('./modules/home')
 const record = require('./modules/record')
 const users = require('./modules/user')
 
-router.use('/', home)
-router.use('/record', record)
-router.use('/users', users)
 
+router.use('/record', authenticator, record)
+router.use('/users', users)
+router.use('/', authenticator, home)
 module.exports = router
